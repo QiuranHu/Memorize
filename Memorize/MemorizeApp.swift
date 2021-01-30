27a6cd01ce4f53fe8b2d3b57cd967f42724bf503
Qiuran Hu <qiuranh@gmail.com>
2021-01-30 15:34:43 +0800

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Qiuran on 2021/1/30.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
