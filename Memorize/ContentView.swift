953d8eaeff80f9721ba5c726e3b7774e0f48c817
Qiuran Hu <qiuranh@gmail.com>
2021-01-31 09:40:34 +0800

    Add 3 cards

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -7,10 +7,34 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct ContentView: View { // ContentView behaves like a View.
+    // The value of the body property is not store in memory.
+    // Every time the system ask for the value of body,
+    // the code in the curly brace gets executed.
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        return HStack {
+            ForEach(0..<4) { index in
+                CardView(isFaceUp: false)
+            }
+        }
+        .padding()
+        .foregroundColor(Color.orange)
+        .font(Font.largeTitle)
+    }
+}
+
+struct CardView: View {
+    var isFaceUp: Bool
+    var body: some View {
+        ZStack {
+            if isFaceUp {
+            RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
+            RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
+            Text("👻")
+            } else {
+            RoundedRectangle(cornerRadius: 10.0).fill()
+            }
+        }
     }
 }
 
